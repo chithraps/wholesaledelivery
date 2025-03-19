d558b6e9cbbaaff0eed0d7e68614ad7080c898c2
chithraps <chithra017@gmail.com>
2025-03-19 15:14:30 +0530

    Added changes in middleware.js

--- src/middleware.js
+++ src/middleware.js
@@ -4,30 +4,45 @@ import { jwtVerify } from "jose";
 export async function middleware(req) {
   let token = req.cookies.get("tdAuth")?.value || req.cookies.get("adminAuth")?.value;
 
-  console.log("Verifying token...");
-  
+  console.log("Verifying token... ", token);
+
   if (!token || token.trim() === "") {
+    if (req.nextUrl.pathname === "/") {
+      return NextResponse.next(); 
+    }
     console.log("Unauthorized: No token provided");
     return NextResponse.redirect(new URL("/", req.url));
   }
 
   try {
+    if (!token.includes(".")) throw new Error("Invalid token format");
+    
     const secret = new TextEncoder().encode(process.env.JWTPRIVATEKEY);
     const { payload } = await jwtVerify(token, secret);
     
     console.log("Token Verified:", payload);
-
-    // Role-based access control
     const role = payload.role;
 
+    
+    if (req.nextUrl.pathname === "/") {
+      console.log("hello")
+      if (role === "truckDriver") {
+        return NextResponse.redirect(new URL("/truckDriver/tdDashboard", req.url));
+      }
+      if (role === "admin") {
+        return NextResponse.redirect(new URL("/admin/dashboard", req.url));
+      }
+    }
+
+    // Role-based access control
     if (req.nextUrl.pathname.startsWith("/truckDriver") && role !== "truckDriver") {
-      console.log("Unauthorized:  cannot access truck driver routes");
+      console.log("Unauthorized: Cannot access truck driver routes");
       return NextResponse.redirect(new URL("/", req.url));
     }
 
     if (req.nextUrl.pathname.startsWith("/admin") && role !== "admin") {
-      console.log("Unauthorized:  cannot access admin routes");
-      return NextResponse.redirect(new URL("/admin", req.url));
+      console.log("Unauthorized: Cannot access admin routes");
+      return NextResponse.redirect(new URL("/", req.url));
     }
 
     return NextResponse.next();
@@ -38,5 +53,5 @@ export async function middleware(req) {
 }
 
 export const config = {
-  matcher: ["/truckDriver/tdDashboard/:path*", "/admin/dashboard:path*"], 
+  matcher: ["/","/truckDriver/:path*", "/admin/:path*"],
 };
