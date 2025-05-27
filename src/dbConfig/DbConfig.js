aaef296db8ac6636c2a2c9c19293d38203ab2d90
chithraps <chithra017@gmail.com>
2025-05-27 10:11:12 +0530

    changes made in dbconfig

--- src/dbConfig/DbConfig.js
+++ src/dbConfig/DbConfig.js
@@ -3,8 +3,7 @@ import mongoose from "mongoose";
 export async function connect() {
   try {
     mongoose.connect(process.env.CONNECTION_STRING, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+      
       dbName: "delivery_management",
     });
     console.log("Database connection is ready !!!");
